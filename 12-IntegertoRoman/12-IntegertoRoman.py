65739839b974ae06ee7be44db7fbf045ea3f2ec3
Syed Ashfaq Ahmed <85483959+AshFury@users.noreply.github.com>
2025-09-08 01:37:42 +0530

    File updated at 08/09/2025, 01:37:42

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,37 +1,21 @@
-# Last updated: 08/09/2025, 01:25:12
+# Last updated: 08/09/2025, 01:37:42
 class Solution:
-    def myAtoi(self, s: str) -> int:
-        s=list(s)
-        neg=False
-        num=0
-        nums=["0","1","2","3","4","5","6","7","8","9"]
+    def strStr(self, haystack: str, needle: str) -> int:
+        l1=list(haystack)
+        l2=list(needle)
         i=0
-        print(s)
-        if s==[]:
-            return 0
-        while s[0]==" ":
-            s.pop(0)
-            if s==[]:
-                return 0
-        print(s)
-        if s[0]=="+" or s[0]=="-":
-            if s[i]=="-":
-                neg=True
-            s.pop(0)
-        for i in s:
-            if i not in nums:
-                break
-            elif int(i)>=0 and int(i)<=9:
-                num+=int(i)
-                num*=10
-        num=num//10
-        if neg:
-            if (-1)*num<(-1)*pow(2,31):
-                return (-1)*pow(2,31)
-            return (-1)*num
-        else:
-            if num>pow(2,31)-1:
-                return pow(2,31)-1
-            return num
+        j=0
+        while i<len(l1):
+            if l1[i]==l2[j]:
+                i+=1
+                j+=1
+                if j==len(l2):
+                    print("hi")
+                    return i-j
+            else:
+                i-=j
+                j=0
+                i+=1
+        return -1
 
         
