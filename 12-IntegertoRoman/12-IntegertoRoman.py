ba61decfc1cc95b9106cb8370ce9ee5c5e45c717
Syed Ashfaq Ahmed <85483959+AshFury@users.noreply.github.com>
2025-09-08 00:06:47 +0530

    File updated at 08/09/2025, 00:06:47

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,24 +1,27 @@
-# Last updated: 07/09/2025, 23:49:35
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# Last updated: 08/09/2025, 00:06:47
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        k = 0
-        curr = head
-        while curr:
-            curr = curr.next
-            k += 1 
-        if k - n == 0:
-            return head.next
-        curr = head
-        for _ in range(1, k - n):
-            curr = curr.next
-        curr.next = curr.next.next
-    
-        return head
-        
+    def reverse(self, x: int) -> int:
+        neg=False
+        if x<0:
+            neg=True
+        x=abs(x)
+        if x>pow(2,31)-1:
+            return 0
+        lis=[]
+        while x>0:
+            lis.append(x%10)
+            x=x//10
+        num=0
+        print(lis)
+        for i in range(0,len(lis)):
+            num+=lis[i]
+            if i!=len(lis)-1:
+                num*=10
+        if neg and num<=pow(2,31)-1:
+            return (-1)*num
+        elif not neg and num<=pow(2,31)-1:
+            return num
+        else:
+            return 0
 
         
