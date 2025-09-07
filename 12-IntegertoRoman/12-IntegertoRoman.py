354ef7494c385768a6ebb73abe4b3f1acb2c8829
Syed Ashfaq Ahmed <85483959+AshFury@users.noreply.github.com>
2025-09-07 23:49:36 +0530

    File updated at 07/09/2025, 23:49:35

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,33 +1,24 @@
-# Last updated: 07/09/2025, 20:21:42
+# Last updated: 07/09/2025, 23:49:35
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        l1=[]
-        l2=[]
-        if list1==None and list2==None:
-            return list1
-        while list1!=None:
-            l1.append(list1.val)
-            list1=list1.next
-        while list2!=None:
-            l2.append(list2.val)
-            list2=list2.next
-        l3=l1+l2
-        l3.sort()
-        new_lis=ListNode()
-        res=new_lis
-        for i in range(0,len(l3)):
-            if i==len(l3)-1:
-                new_lis.val=l3[i]
-                return res
-            new_lis.val=l3[i]
-            new_lis.next=ListNode()
-            new_lis=new_lis.next
-        return res
-
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        k = 0
+        curr = head
+        while curr:
+            curr = curr.next
+            k += 1 
+        if k - n == 0:
+            return head.next
+        curr = head
+        for _ in range(1, k - n):
+            curr = curr.next
+        curr.next = curr.next.next
+    
+        return head
+        
 
         
