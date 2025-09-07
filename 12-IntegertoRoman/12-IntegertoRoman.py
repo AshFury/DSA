851d861ad754328298527305611c598b8829bb7d
Syed Ashfaq Ahmed <85483959+AshFury@users.noreply.github.com>
2025-09-08 01:25:13 +0530

    File updated at 08/09/2025, 01:25:12

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,27 +1,37 @@
-# Last updated: 08/09/2025, 00:06:47
+# Last updated: 08/09/2025, 01:25:12
 class Solution:
-    def reverse(self, x: int) -> int:
+    def myAtoi(self, s: str) -> int:
+        s=list(s)
         neg=False
-        if x<0:
-            neg=True
-        x=abs(x)
-        if x>pow(2,31)-1:
-            return 0
-        lis=[]
-        while x>0:
-            lis.append(x%10)
-            x=x//10
         num=0
-        print(lis)
-        for i in range(0,len(lis)):
-            num+=lis[i]
-            if i!=len(lis)-1:
-                num*=10
-        if neg and num<=pow(2,31)-1:
-            return (-1)*num
-        elif not neg and num<=pow(2,31)-1:
-            return num
-        else:
+        nums=["0","1","2","3","4","5","6","7","8","9"]
+        i=0
+        print(s)
+        if s==[]:
             return 0
+        while s[0]==" ":
+            s.pop(0)
+            if s==[]:
+                return 0
+        print(s)
+        if s[0]=="+" or s[0]=="-":
+            if s[i]=="-":
+                neg=True
+            s.pop(0)
+        for i in s:
+            if i not in nums:
+                break
+            elif int(i)>=0 and int(i)<=9:
+                num+=int(i)
+                num*=10
+        num=num//10
+        if neg:
+            if (-1)*num<(-1)*pow(2,31):
+                return (-1)*pow(2,31)
+            return (-1)*num
+        else:
+            if num>pow(2,31)-1:
+                return pow(2,31)-1
+            return num
 
         
