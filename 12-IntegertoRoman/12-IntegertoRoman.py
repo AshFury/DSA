02fc280b85ba1ed172aef991c739529affebe1b6
Syed Ashfaq Ahmed <85483959+AshFury@users.noreply.github.com>
2025-09-08 09:45:35 +0530

    File updated at 08/09/2025, 09:45:34

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,21 +1,16 @@
-# Last updated: 08/09/2025, 01:37:42
+# Last updated: 08/09/2025, 09:45:34
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        l1=list(haystack)
-        l2=list(needle)
-        i=0
-        j=0
-        while i<len(l1):
-            if l1[i]==l2[j]:
-                i+=1
-                j+=1
-                if j==len(l2):
-                    print("hi")
-                    return i-j
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid=left+(right-left)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                right=mid-1
             else:
-                i-=j
-                j=0
-                i+=1
-        return -1
+                left=mid+1
+        return left
 
         
