654854ab3ac2d7e76cb779190eabbe3aa4cbdee5
Syed Ashfaq Ahmed <85483959+AshFury@users.noreply.github.com>
2025-09-09 10:27:52 +0530

    File updated at 09/09/2025, 10:27:52

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,16 +1,16 @@
-# Last updated: 09/09/2025, 10:22:06
+# Last updated: 09/09/2025, 10:27:52
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        res=0
-        start=False
-        for i in range(len(s)-1,-1,-1):
-            print(s[i])
-            if s[i]==" " and not start:
-                continue
-            elif s[i]==" " and start:
-                return res
-            else:
-                start=True
-                res+=1
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num=0
+        for i in digits:
+            num+=i
+            num*=10
+        num=num//10
+        num+=1
+        res=[]
+        while num>0:
+            res.append(num%10)
+            num=num//10
+        print(res)
+        res.reverse()
         return res
-        
