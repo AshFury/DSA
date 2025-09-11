886beacaefadfdca64cab9bdd7cd8f948d8a1110
Syed Ashfaq Ahmed <85483959+AshFury@users.noreply.github.com>
2025-09-11 09:37:56 +0530

    File updated at 11/09/2025, 09:37:56

--- 67-AddBinary/67-AddBinary.py
+++ 67-AddBinary/67-AddBinary.py
@@ -1,78 +1,16 @@
-# Last updated: 11/09/2025, 09:35:16
+# Last updated: 11/09/2025, 09:37:56
 class Solution:
-    def addBinary(self, a: str, b: str) -> str:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num=0
+        for i in digits:
+            num+=i
+            num*=10
+        num=num//10
+        num+=1
         res=[]
-        max_len=max(len(a),len(b))
-        carry=0
-        for i in range(0,max_len):
-            a_pos=(len(a)-1)-i
-            b_pos=(len(b)-1)-i
-            if a_pos>=0 and b_pos>=0:
-                if a[a_pos]=="1" and b[b_pos]=="1":
-                    if carry==1:
-                        print("here")
-                        res.append(1)
-                    else:
-                        print("here")
-                        res.append(0)
-                    carry=1
-                elif a[a_pos]=="1" and b[b_pos]=="0":
-                    if carry==1:
-                        res.append(0)
-                        carry=1
-                    else:
-                        res.append(1)
-                        carry=0
-                elif a[a_pos]=="0" and b[b_pos]=="1":
-                    if carry==1:
-                        res.append(0)
-                        carry=1
-                    else:
-                        res.append(1)
-                        carry=0
-                else:
-                    if carry==1:
-                        res.append(1)
-                    else:
-                        res.append(0)
-                    carry=0
-            elif a_pos>=0 and b_pos<0:
-                if a[a_pos]=="1":
-                    if carry==1:
-                        res.append(0)
-                        carry=1
-                    else:
-                        res.append(1)
-                        carry=0
-                else:
-                    if carry==1:
-                        res.append(1)
-                        carry=0
-                    else:
-                        res.append(0)
-                        carry=0
-            elif b_pos>=0 and a_pos<0:
-                if b[b_pos]=="1":
-                    if carry==1:
-                        res.append(0)
-                        carry=1
-                    else:
-                        res.append(1)
-                        carry=0
-                else:
-                    if carry==1:
-                        res.append(1)
-                        carry=0
-                    else:
-                        res.append(0)
-                        carry=0
-        if carry==1:
-            res.append(1)
-            carry=0
+        while num>0:
+            res.append(num%10)
+            num=num//10
         print(res)
         res.reverse()
-        print(len(a))
-        s=""
-        for i in res:
-            s=s+str(i)
-        return s
+        return res
